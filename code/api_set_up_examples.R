903d609162e0727d88d1453bf71b418fe235712c
emhwebb <31523239+emhwebb@users.noreply.github.com>
2018-04-30 18:41:50 -0700

    updating

--- code/api_set_up_examples.R
+++ code/api_set_up_examples.R
@@ -3,7 +3,7 @@ library(jsonlite)
 library(xml2)
 library(rvest)
 
-NEWSAPI_KEY <- # insert your api here
+NEWSAPI_KEY <- #your newsapi key goes here
 
 src <- get_sources(language = "en")
 
@@ -26,6 +26,36 @@ test_url <- content_test[["articles"]][[5]][["url"]]
 test_html <- read_html(test_url)
 
 #.js-article__body works for filtering body of text in guardian uk articles
-test_html %>%
+test_text <- test_html %>%
   html_node(".js-article__body") %>%
   html_text()
+
+get_newsapi_url <- function(url, q, path, sources, apiKey){
+  query <- list(q = q, sources = sources, apiKey = apiKey)
+  urls <- GET(url = url, path = path, query = query)
+  content(urls)
+}
+
+get_guardian_html_text <- function(i, url_list){
+  article_url <- url_list[["articles"]][[i]][["url"]]
+  text <- read_html(article_url) %>% 
+    html_node(".js-article__body") %>%
+    html_text()
+  publish.date <- url_list[["articles"]][[i]][["publishedAt"]]
+  c(text, publish.date)
+}
+
+get_politico_html_text <- function(i, url_list){
+  article_url <- url_list[["articles"]][[i]][["url"]]
+  text <- read_html(article_url) %>% 
+    html_node(".story-text") %>%
+    html_text()
+  publish.date <- url_list[["articles"]][[i]][["publishedAt"]]
+  c(text, publish.date)
+}
+
+#example when our search is string cheese
+lapply(1:length(content_test[["articles"]]), get_guardian_html_text, content_test)
+
+get_newsapi_url(url = "https://newsapi.org", q = "string cheese", 
+                path = "/v2/everything", sources = "the-guardian-uk", apiKey = NEWSAPI_KEY) 
