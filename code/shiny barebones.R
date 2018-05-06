df6dd1a81dce5cef4c48b25dc3a9eeb070bebdd5
Henry Benjamin Knopf <henknopf@rstudio-dev.reed.edu>
2018-05-06 16:49:59 -0700

    added more depth to design

--- code/shiny barebones.R
+++ code/shiny barebones.R
@@ -1,20 +1,41 @@
 library(shiny)
 ui <- fluidPage(
-  headerPanel(
-  plotOutput("timeseries")
-               ),
+               
+  mainPanel(
+    plotOutput("timeseries"),
+    tableOutput("sentiment"),
+          ),
   
-  sidebarPanel(
+ sidebarLayout( 
+   sidebarPanel(
     selectInput(inputId = 'sources', label = 'News Sources'),
+    htmlOutput("selectUI"),
+    textInput(inputId = 'news_search', label = 'Search News',width = "400px"),
+    submitButton(text = "Update Page",icon = "refresh")
+               )
+             )
+ 
     #must have list of news sources, write the code for the timeseries plot connected to the inputID. Similarly, sentiment table output
   #  below will reactively change based on the dataset. Must code in a reactive for that server-side. 
   
-  tableOutput("sentiment")
-             )
+  
+  
   
 )
 
 server < - function(input, output) {
+  
+  searchResult<- reactive({
+    subset(news_text, grep(pattern = input$news_search, x = news_text$text, ignore.case = T ))
+  })
+  output$selectUI <- reactive({ 
+    sources %>%
+      filter(sources %in% input$sources)
+  })
+  
+  output$searchResults <- renderTable({ 
+    searchResult[,1]
+  })
   output$timeseries <- renderPlot({
     timeseries
   }) 
